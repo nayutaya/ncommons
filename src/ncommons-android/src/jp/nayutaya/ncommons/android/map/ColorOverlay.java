e587223833d8d90367a976f140e43c3009a74d1f
Yuya Kato <yuyakato@gmail.com>
2012-05-20 15:34:32 +0900

    背景色をオーバーレイ表示するColorOverlayクラスを追加

---
+++ src/ncommons-android/src/jp/nayutaya/ncommons/android/map/ColorOverlay.java
@@ -0,0 +1,26 @@
+
+package jp.nayutaya.ncommons.android.map;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+
+public class ColorOverlay extends Overlay
+{
+    private final Paint paint;
+
+    public ColorOverlay(final Paint paint)
+    {
+        this.paint = paint;
+    }
+
+    @Override
+    public void draw(final Canvas canvas, final MapView mapView, final boolean shadow)
+    {
+        if ( shadow )
+        {
+            canvas.drawRect(0.0f, 0.0f, mapView.getWidth(), mapView.getHeight(), this.paint);
+        }
+    }
+}
