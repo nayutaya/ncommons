c793b4b0543c5363583f350b49131d3fdd14484a
Yuya Kato <yuyakato@gmail.com>
2012-05-25 00:14:35 +0900

    表示/非表示を切り替える処理を追加

--- src/ncommons-android/src/jp/nayutaya/ncommons/android/map/ColorOverlay.java
+++ src/ncommons-android/src/jp/nayutaya/ncommons/android/map/ColorOverlay.java
@@ -9,15 +9,18 @@ import com.google.android.maps.Overlay;
 public class ColorOverlay extends Overlay
 {
     private final Paint paint;
+    private boolean visible;
 
     public ColorOverlay()
     {
         this.paint = new Paint();
+        this.setVisible(false);
     }
 
     public ColorOverlay(final Paint paint)
     {
         this.paint = paint;
+        this.setVisible(false);
     }
 
     public Paint getPaint()
@@ -25,10 +28,20 @@ public class ColorOverlay extends Overlay
         return this.paint;
     }
 
+    public boolean isVisible()
+    {
+        return this.visible;
+    }
+
+    public void setVisible(final boolean visible)
+    {
+        this.visible = visible;
+    }
+
     @Override
     public void draw(final Canvas canvas, final MapView mapView, final boolean shadow)
     {
-        if ( shadow )
+        if ( this.isVisible() && shadow )
         {
             canvas.drawRect(0.0f, 0.0f, mapView.getWidth(), mapView.getHeight(), this.getPaint());
         }
